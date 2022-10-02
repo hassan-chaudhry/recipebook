9f24b7bf3d45ba6498dea83710fe5a6458deee4a
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-10-02 00:30:57 -0400

    fixed retrieval design

--- MainProgram.java
+++ MainProgram.java
@@ -1,16 +1,16 @@
-import java.util.*;
-import java.io.*;
+// import java.util.*;
+// import java.io.*;
 
-public class MainProgram {
+// public class MainProgram {
     
-    public static ArrayList<Recipes> recipeList = new ArrayList<Recipes> ();
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+//     public static ArrayList<Recipes> recipeList = new ArrayList<Recipes> ();
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
 
-        RecipeRetrieval data = new RecipeRetrieval();
-        ArrayList<Recipes> hold = data.LoadRecipes();
-        recipeList = hold;
-        RecipeRetrieval recipeRetrieval = new RecipeRetrieval();
-        recipeRetrieval.retrievalInterface(input, recipeList);
-    }
-}
+//         RecipeRetrieval data = new RecipeRetrieval();
+//         ArrayList<Recipes> hold = data.LoadRecipes();
+//         recipeList = hold;
+//         RecipeRetrieval recipeRetrieval = new RecipeRetrieval();
+//         recipeRetrieval.retrievalInterface(input, recipeList);
+//     }
+// }
