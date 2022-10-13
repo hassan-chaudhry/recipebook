e8384c793da12f3e361b701eea20a7a64a8a32e2
hassan-chaudhry <hc2843@nyu.edu>
2022-10-13 16:07:48 -0400

    updated GUI with card layout in Java Swing

--- RecipeBookGUI.java
+++ RecipeBookGUI.java
@@ -1,34 +1,107 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-class RecipeBookGUI{
-    public static void main(String args[]){
+public class RecipeBookGUI extends JFrame {
 
-       // create frame
-       JFrame frame = new JFrame("Recipe Book");
-       frame.setSize(450,400);
-       frame.getContentPane().setBackground(new java.awt.Color(255, 80, 80));
+	// define variables
+	private static CardLayout cardLayout;
+	private static JFrame frame;
+	private static JPanel cardPanel, pMain, pCreation, pRetrieval;
+	private static JLabel lMain, lCreation, lRetrieval;
+	private static JButton bBack1, bBack2, bCreation, bRetrieval, bBrowse, bSearch;
+	
+	public static void main(String args[]) {
+		RecipeBookGUI rbg = new RecipeBookGUI();
+		rbg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		rbg.setVisible(true); 
 
-       frame.setVisible(true);
-       frame.setLayout(null); 
+		cardLayout.first(cardPanel);
+	}
 
-       // create header "Recipe Book"
-       JLabel header = new JLabel("Recipe Book", SwingConstants.CENTER);
-       header.setForeground(Color.WHITE);
-       header.setBounds(150, -25, 150, 150);
-       header.setFont(new Font("Verdana", Font.PLAIN, 18));
-       frame.add(header);
+	public RecipeBookGUI() {
+		// set frame
+		setTitle("Recipe Book");
+		setSize(600, 300);
+		
+		// set CardLayout
+		cardPanel = new JPanel(); 
+		cardLayout = new CardLayout(); 
+		cardPanel.setLayout(cardLayout);
 
-       // create button "Create Recipe"
-       JButton creationButton = new JButton("Create Recipe");
-       creationButton.setBounds(150, 100, 150, 75);
-       frame.add(creationButton);
+		// main "Recipe Book" page
+		pMain = new JPanel(); // create new panel
+		pMain.setBackground(new java.awt.Color(255, 80, 80));
 
-       // create button "View Recipes"
-       JButton retrievalButton = new JButton("View Recipes");
-       retrievalButton.setBounds(150, 200, 150, 75);
-       frame.add(retrievalButton); 
-       
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+		lMain = new JLabel("Recipe Book"); // create new label
+		lMain.setForeground(Color.WHITE);
+
+		bCreation = new JButton("Create Recipe"); // create new button
+		bRetrieval = new JButton("Retrieve Recipe"); // create new button
+
+		pMain.add(lMain); // add label to panel
+		pMain.add(bCreation); // add button to panel
+		pMain.add(bRetrieval); // add button to panel
+
+		// "Recipe Creation" page
+		pCreation = new JPanel();
+		pCreation.setBackground(new java.awt.Color(44,238,144));
+
+		lCreation = new JLabel("Recipe Creation");
+		lCreation.setForeground(Color.WHITE);
+		bBack1 = new JButton("Go Back");
+
+		pCreation.add(lCreation);
+		pCreation.add(bBack1);
+
+		// "Recipe Retrieval" page
+		pRetrieval = new JPanel();
+		pRetrieval.setBackground(new java.awt.Color(66, 135, 245));
+
+		lRetrieval = new JLabel("Recipe Retrieval");
+		lRetrieval.setForeground(Color.WHITE);
+
+		bBrowse = new JButton("Browse Recipes");
+		bSearch = new JButton("Search For A Recipe");
+		bBack2 = new JButton("Go Back");
+
+		pRetrieval.add(lRetrieval);
+		pRetrieval.add(bBrowse);
+		pRetrieval.add(bSearch);
+		pRetrieval.add(bBack2);
+
+		// add every other panel to main "cardPanel" panel
+		cardPanel.add(pMain, "Recipe Book");
+		cardPanel.add(pCreation, "Recipe Creation");
+		cardPanel.add(pRetrieval, "Recipe Retrieval");
+
+		// display "Recipe Book" page
+		getContentPane().add(cardPanel, BorderLayout.CENTER);
+
+		// switching pages
+		bBack1.addActionListener(new ActionListener() {  
+			public void actionPerformed(ActionEvent ae) {  
+				cardLayout.show(cardPanel, "" + "Recipe Book");
+			}		  
+		}); 
+
+		bBack2.addActionListener(new ActionListener() {  
+			public void actionPerformed(ActionEvent ae) {  
+				cardLayout.show(cardPanel, "" + "Recipe Book");
+			}		  
+		}); 
+
+		bCreation.addActionListener(new ActionListener() {  
+			public void actionPerformed(ActionEvent ae) {  
+				cardLayout.show(cardPanel, "" + "Recipe Creation");
+			}		  
+		});   
+
+		bRetrieval.addActionListener(new ActionListener() {  
+			public void actionPerformed(ActionEvent ae) {  
+				cardLayout.show(cardPanel, "" + "Recipe Retrieval");
+			}		  
+		});  
+
+	}
 }
