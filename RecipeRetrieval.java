72b355e1eff2603a4656ab62b98a45929fe170f5
G <gg2484@nyu.edu>
2022-10-15 19:12:13 -0400

    added data validation and fixed bug

--- RecipeRetrieval.java
+++ RecipeRetrieval.java
@@ -173,7 +173,10 @@ public class RecipeRetrieval {
             obj = new Scanner(System.in);
     
             System.out.println("If there's a recipe you want to read in details, please enter its number. Otherwize please enter 0.\n");
-             choice = obj.nextInt();
+            choice = -1;
+            if (obj.hasNextInt()){
+                choice = obj.nextInt();
+            } 
             
             if (choice < 0 || choice >list.size()){
                 System.out.println("Invalid Input. Please try again.\n");
